aba2aee32d0ddc50e12708c97fc0fec0c50ab687
Muhammad Shakirin Samin <cybersharky94@gmail.com>
2019-11-21 10:19:41 +0800

    Add app config

---
+++ config/app.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    
+
+    'providers' => [
+
+        
+
+        /*
+         * Application Service Providers...
+         */
+        
+        App\Providers\HelperServiceProvider::class,
+
+    ],
+
+];
